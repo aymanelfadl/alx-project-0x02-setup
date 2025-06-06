7b45807c616b385546fa5d51fefc69038a14b0c7
ayman elfadl <aymanelfadl16@gmail.com>
2025-06-06 18:59:53 +0100

    ok

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
 import PostCard from "@/components/common/PostCard";
 import Layout from "@/components/layout/Layout";
 import { PostProps } from "@/interfaces";
 
-interface APIPost {
-  title: string;
-  body: string;
-  userId: number;
-  id: number;
-}
-
-export default function PostsPage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then((res) => res.json())
-      .then((data: APIPost[]) => {
-        const formatted = data.map((post) => ({
-          title: post.title,
-          content: post.body,
-          userId: post.userId,
-        }));
-        setPosts(formatted);
-      });
-  }, []);
-
+export default function PostsPage({ posts }: { posts: PostProps[] }) {
   return (
     <Layout>
       <div className="p-6 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -40,4 +17,21 @@ export default function PostsPage() {
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+  const data = await res.json();
+
+  const posts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
