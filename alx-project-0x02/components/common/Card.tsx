fc02e87c898a536e8ceea5eca2f93525d4b1afe0
ayman elfadl <aymanelfadl16@gmail.com>
2025-06-06 18:47:17 +0100

    ok

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -2,7 +2,7 @@ import { CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg border-gray-300 border-t-2 border-r-2  p-4 w-full max-w-md">
+    <div className="bg-white shadow-md  hover:bg-gray-100 rounded-lg border-gray-300 border-t-2 border-r-2  p-4 w-full max-w-md">
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700">{content}</p>
     </div>
